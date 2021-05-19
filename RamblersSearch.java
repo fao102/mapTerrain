a22ef1fa1c99c602fa557103f7bb1b42e872434d
File <fiajan1602@gmail.com>
2021-05-19 15:25:39 +0100

    Ramblers updates

--- RamblersSearch.java
+++ RamblersSearch.java
@@ -1,3 +1,27 @@
-public class RamblersSearch {
-    
+import java.util.*;
+
+public class RamblersSearch extends Search {
+
+  private Carta map; //map we're searching
+  private String goal; //goal city
+
+  public Carta getMap(){
+    return map;
+  }
+  public String getGoal(){
+    return goal;
+  }
+
+  public RamblersSearch(Carta m, String g){
+    map=m;
+    goal=g;
+  }
 }
+
+
+
+
+
+
+
+
